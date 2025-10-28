854b54d3296a44eb8415a8723cad85ab10fb772c
Hama1234567mm <166730368+Hama1234567mm@users.noreply.github.com>
2025-10-28 10:23:56 -1100

    Update 1known.py

--- 1known.py
+++ 1known.py
@@ -133,4 +133,5 @@ def create_directory(client_name):
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+
+    app.run(host='0.0.0.0', port=10000, debug=True)
